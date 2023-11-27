4c3a80d41fd531481f4b3bc6566dd6c1a7edcf41
Manish Balamurugan <manishbalamurugan@gmail.com>
2023-11-27 17:24:15 -0500

    [INIT] Mongo Connection & API Endpoints

--- voters/api/server.js
+++ voters/api/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Express + MongoDB Setup
 const uri = "mongodb+srv://manish:Voters4260@voters.eucgeq2.mongodb.net/?retryWrites=true&w=majority";
-const db = "Voters"
+
 
 const port = process.env.PORT || 4200;
 
@@ -14,6 +14,10 @@ const client = new mongodb.MongoClient(uri);
 
 app.use(cors());
 app.use(express.json());
+
+app.listen(4200, () => {
+    console.log('Server running on port 4200');
+});
 
 // Connect to MongoDB
 client.connect()
@@ -24,4 +28,16 @@ client.connect()
     console.error('Failed to connect to MongoDB', err);
   });
 
+const db = client.db("voting");
+
 app.use(express.json());
+
+app.get('/candidates',  async (req, res) => {
+    try{
+        const candidates = await db.collection("candidates").find().toArray()
+        console.log(candidates);
+        res.end(JSON.stringify(candidates));
+    } catch (error) {
+        res.status(500).json({success: false, error:'Internal server error'});
+    }
+})
